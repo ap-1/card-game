b6eef3a5c248a11c6a96894687dbbc38c5fb4f27
Anish Pallati <anishp@duck.com>
2022-11-28 14:03:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# card-game
+
+Play card games with friends!
+
+## Development
+
+```bash
+pnpm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import {
+	createRouteConfig,
+	createReactRouter,
+	RouterProvider,
+	Outlet,
+} from "@tanstack/react-router";
+
+import Index from "./routes/Index";
+import Navbar from "./components/Navbar";
+
+const root = createRouteConfig();
+const indexRoute = root.createRoute({
+	path: "/",
+	component: Index,
+});
+
+const routeConfig = root.addChildren([indexRoute]);
+const router = createReactRouter({ routeConfig });
+
+export default function App() {
+	return (
+		<RouterProvider router={router}>
+			<Navbar />
+			<Outlet />
+		</RouterProvider>
+	);
+}
